79f41aa6f80f2581171f14f8af1996ed12b7ae9d
João Victor Passos Canavez <sap0000212@aluno.firjansenaisesi.com.br>
2026-06-18 10:46:49 -0300

    Primeira aula de Pyton no FIRJAN SENAI

---
+++ aula-01/desafio02.py
@@ -0,0 +1,9 @@
+from desafio01 import nome
+
+print("Agora preciso que você me informe seu aniversário.")
+
+dia = input("Qual o dia? ")
+mes = input("Qual o mês? ")
+ano = input("Qual o ano? ")
+
+print(f"Perfeito, {nome}! Você nasceu no dia {dia} de {mes} de {ano}.")
---
+++ aula-01/ex002.py
@@ -0,0 +1,2 @@
+print(7 + 4) # resultado: 11
+print("7" + "4") # resultado: 74
